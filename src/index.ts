32b4c6fe128bfd29449b21269ff1cf90a3fd75cf
卡晨 <guangbo.hgb@antfin.com>
2019-02-19 15:48:35 +0800

    bug fix and upgrade react dependencies

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,8 @@ import {useEffect, useRef, useState} from 'react'
 
 export function useChanging<T>(value: T, timeout: number) {
   const [changing, setChanging] = useState(true)
-  const lastValueRef = useRef<T>(undefined)
-  if (lastValueRef.current !== value) {
-    lastValueRef.current = value
-    if (!changing) setChanging(true)
-  }
   useEffect(() => {
+    if (!changing) setChanging(true)
     const id = setTimeout(() => {
       setChanging(false)
     }, timeout)
